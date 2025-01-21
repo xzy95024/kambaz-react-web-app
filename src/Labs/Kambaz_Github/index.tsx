2b1e24c1b46fd750d72090329a44a1217850167c
xzy95024 <865389193@qq.com>
2025-01-20 20:39:10 -0500

    open github in new tab

--- src/Labs/Kambaz_Github/index.tsx
+++ src/Labs/Kambaz_Github/index.tsx
@@ -1,7 +1,9 @@
 export default function Kambaz_Github() {
     return (
         <div>
-            <a href="https://github.com/xzy95024/kambaz-react-web-app/tree/main">Visit my Github Repository!</a>
+            <a href="https://github.com/xzy95024/kambaz-react-web-app/tree/main" target="_blank">
+                Visit my Github Repository!
+            </a>
         </div>
     );
 }
