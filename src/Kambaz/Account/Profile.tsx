d3453e7ad487c74dcf253b9b84cc4698e2746ee1
xzy95024 <89833148+xzy95024@users.noreply.github.com>
2025-03-26 18:20:05 -0400

    Update Profile.tsx

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -23,25 +23,33 @@ export default function Profile() {
             {profile && (
                 <div>
                     <FormControl defaultValue={profile.username} id="wd-username" className="mb-2"
-                                 onChange={(e) => setProfile({ ...profile, username:  e.target.value })}/>
+                                 onChange={(e) => setProfile({...profile, username: e.target.value})}/>
                     <FormControl defaultValue={profile.password} id="wd-password" className="mb-2"
-                                 onChange={(e) => setProfile({ ...profile, password:  e.target.value })}/>
+                                 onChange={(e) => setProfile({...profile, password: e.target.value})}/>
                     <FormControl defaultValue={profile.firstName} id="wd-firstname" className="mb-2"
-                                 onChange={(e) => setProfile({ ...profile, firstName: e.target.value })}/>
+                                 onChange={(e) => setProfile({...profile, firstName: e.target.value})}/>
                     <FormControl defaultValue={profile.lastName} id="wd-lastname" className="mb-2"
-                                 onChange={(e) => setProfile({ ...profile, lastName:  e.target.value })}/>
+                                 onChange={(e) => setProfile({...profile, lastName: e.target.value})}/>
                     <FormControl defaultValue={profile.dob} id="wd-dob" className="mb-2"
-                                 onChange={(e) => setProfile({ ...profile, dob: e.target.value })} type="date"/>
+                                 onChange={(e) => setProfile({...profile, dob: e.target.value})} type="date"/>
                     <FormControl defaultValue={profile.email} id="wd-email" className="mb-2"
-                                 onChange={ (e) => setProfile({ ...profile, email: e.target.value })}/>
-                    <select onChange={(e) => setProfile({ ...profile, role:  e.target.value })}
-                            className="form-control mb-2" id="wd-role">
-                        <option value="USER">User</option>            <option value="ADMIN">Admin</option>
-                        <option value="FACULTY">Faculty</option>      <option value="STUDENT">Student</option>
+                                 onChange={(e) => setProfile({...profile, email: e.target.value})}/>
+                    <select
+                        value={profile.role}
+                        onChange={(e) => setProfile({...profile, role: e.target.value})}
+                        className="form-control mb-2"
+                        id="wd-role"
+                        disabled={currentUser?.role !== "FACULTY"}  //
+                    >
+                        <option value="USER">User</option>
+                        <option value="ADMIN">Admin</option>
+                        <option value="FACULTY">Faculty</option>
+                        <option value="STUDENT">Student</option>
                     </select>
                     <Button onClick={signout} className="w-100 mb-2" id="wd-signout-btn">
                         Sign out
                     </Button>
                 </div>
             )}
-        </div>);}
+        </div>);
+}
