c871ea13b06fa49982810cc344c1c576533583d1
xzy95024 <865389193@qq.com>
2025-04-02 06:22:53 -0400

    netlify fk u

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,14 +1,3 @@
-declare module "../../Database/index.js" {
-    const courses: any;
-    const modules: any;
-    const assignments: any;
-    const users: any;
-    const enrollments: any;
-
-    export { courses, modules, assignments, users, enrollments };
-}
-
-
 declare module "../Database/index.js" {
     const courses: any;
     const modules: any;
