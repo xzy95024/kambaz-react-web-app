672e052d17f0ebbf7b83e7d509c0cc0330be2b61
xzy95024 <865389193@qq.com>
2025-04-02 06:06:01 -0400

    netlify plzgit add .

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -8,15 +8,6 @@ declare module "../../Database/index.js" {
     export { courses, modules, assignments, users, enrollments };
 }
 
-declare module "../../Database/index.js" {
-    const courses: any;
-    const modules: any;
-    const assignments: any;
-    const users: any;
-    const enrollments: any;
-
-    export { courses, modules, assignments, users, enrollments };
-}
 
 declare module "../Database/index.js" {
     const courses: any;
