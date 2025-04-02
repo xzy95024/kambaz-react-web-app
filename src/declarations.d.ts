ce8db4bd56b49fcb78686fd25b9b1b86ccc00874
xzy95024 <865389193@qq.com>
2025-04-02 06:02:14 -0400

    netlify3

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,3 +7,23 @@ declare module "../../Database/index.js" {
 
     export { courses, modules, assignments, users, enrollments };
 }
+
+declare module "../../Database/index.js" {
+    const courses: any;
+    const modules: any;
+    const assignments: any;
+    const users: any;
+    const enrollments: any;
+
+    export { courses, modules, assignments, users, enrollments };
+}
+
+declare module "../Database/index.js" {
+    const courses: any;
+    const modules: any;
+    const assignments: any;
+    const users: any;
+    const enrollments: any;
+
+    export { courses, modules, assignments, users, enrollments };
+}
