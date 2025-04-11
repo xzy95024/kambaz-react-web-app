9b61d6c6077735e53a8db0e71a0fab1274c14c47
xzy95024 <865389193@qq.com>
2025-04-11 17:53:16 -0400

    netlify debug

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -57,7 +57,7 @@ export default function Users() {
                 Users
             </button>
 
-            <FormControl onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
+            <FormControl value={name} onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
                          className="float-start w-25 me-2 wd-filter-by-name"/>
 
             <select value={role} onChange={(e) => filterUsersByRole(e.target.value)}
